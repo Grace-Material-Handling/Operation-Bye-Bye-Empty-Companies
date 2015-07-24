80dc9eb122fca5283693aeec9865fd9d909c1fa3
christianjuth <cjuth2@gmail.com>
2015-07-24 11:17:13 -0400

    jenkins console output fix

---
+++ app.rb
@@ -0,0 +1,28 @@
+# app.rb
+
+STDOUT.sync = true
+
+# login to nutshell
+$nutshell = NutshellCrmAPI::Client.new($username, $apiKey)
+
+
+companies = []
+i = 0
+loop do
+	i+=1
+	response = $nutshell.find_accounts([], nil, nil, 100, i, false)
+	break if response.length == 0
+	companies.concat(response)
+end
+
+
+
+companies_without_people = []
+companies.each do |company|
+	if company["contacts"].length == 0
+		companies_without_people.push(company)
+	end
+end
+
+
+puts "You have #{companies_without_people.length} companies without people."
